65ef128f8083faf25fcfe6e7df4e89dc6b0e0fe8
Markus Amano <markus.a.amano@gmail.com>
2021-02-08 20:15:39 -0600

    New Board Struct

--- src/Sudoku.jl
+++ src/Sudoku.jl
@@ -1,25 +1,13 @@
 module Sudoku
 
-# 9x9 Board Object
-# The board must have 81 tiles and each tile must be either a ' ' blank or a numeral.
-# 
-
 struct Board
-	current
-	solution
+	tiles::Array{Int64,2}
 end
 
-# There must be a function for boards that sees if the board is in a valid states
-#
-# There must be a function that tests if the board is in a win state.
-#
-# User input
-# the user must be able to change the board state, so there must be a function that 
-# changes a tile on the board.
-#
-# The Board must have an unique answer (that is "hidden")
-#
-# AI
-# There must be a function that that finds the "next step" (this can be a hint feature)
-# 
+function Board(; state=:unknown)
+	state == :unknown && Board(zeros(Int64, 9, 9))
+end
+
+
+
 end # module
