403536e5481d9687d6a421397ec4525cdcb6b7f4
Markus Amano <markus.a.amano@gmail.com>
2021-02-08 20:30:12 -0600

    add bare funcs

--- src/Sudoku.jl
+++ src/Sudoku.jl
@@ -8,6 +8,28 @@ function Board(; state=:unknown)
 	state == :unknown && Board(zeros(Int64, 9, 9))
 end
 
+function get_row(board::Board, row_num::Int64)
+	error("TBD")
+end
 
+function get_column(board::Board, col_num::Int64)
+	error("TBD")
+end
+
+function get_block(board::Board, block_num::Int64)
+	error("TBD")
+end
+
+function is_valid(board::Board)
+	error("TBD")
+end
+
+function set_element!(board::Board,row_num::Int64,col_num::Int64)
+	error("TBD")
+end
+
+function possible_moves(board::Board,row_num::Int64,col_num::Int64)
+	error("TBD")
+end
 
 end # module
