812202d06f7e4defb4a0d08eef36ee6da2f49cc5
Markus Amano <markus.a.amano@gmail.com>
2021-02-08 21:42:18 -0600

    implemented get_row

--- src/Sudoku.jl
+++ src/Sudoku.jl
@@ -13,14 +13,14 @@ function get_element(board::Board, row_num::Int64,col_num::Int64)
 end
 
 function get_row(board::Board, row_num::Int64)
-	error("TBD")
+	board.tiles[row_num,:]
 end
 
 function get_column(board::Board, col_num::Int64)
 	error("TBD")
 end
 
-function get_block(board::Board, block_num::Int64)
+function get_block(board::Board, row_num::Int64,col_num::Int64)
 	error("TBD")
 end
 
@@ -43,7 +43,9 @@ function possible_moves(board::Board,row_num::Int64,col_num::Int64)
 
 	element == 0 && return pos_moves
 
-	return [i for i ∈ 1:9 if i∉get_row(board,row_num) && i∉get_column(board,col_num) && i∉get_block(board,row_num, col_num)]
+	return [i for i ∈ 1:9 if i∉get_row(board, row_num) 
+													 && i∉get_column(board, col_num) 
+													 && i∉get_block(board, row_num, col_num)]
 end
 
 function solve!(board::Board)
