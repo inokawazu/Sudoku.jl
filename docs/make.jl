3129445dd2295a18431db7a1eeecff7b3002100f
Markus Amano <markus.a.amano@gmail.com>
2021-02-11 21:06:03 -0600

    moved the load_path change

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,6 @@
+
+push!(LOAD_PATH,"../src/")
+
 using Documenter
 using Sudoku
 
@@ -6,8 +9,6 @@ makedocs(
     format = Documenter.HTML(),
     modules = [Sudoku]
 )
-
-push!(LOAD_PATH,"../src/")
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
